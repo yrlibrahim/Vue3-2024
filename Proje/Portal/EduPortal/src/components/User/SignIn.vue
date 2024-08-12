fef6ccef6ab5a750d575db4ee6fca792884181cc
Ibrahim Yerli <yrlibrahim25@gmail.com>
2024-08-12 17:59:25 +0300

    action added

--- Proje/Portal/EduPortal/src/components/User/SignIn.vue
+++ Proje/Portal/EduPortal/src/components/User/SignIn.vue
@@ -1,36 +1,62 @@
 <template>
   <div class="sign-in-container">
-    <Form class="sign-in-form row gy-4">
-      <h2>Giriş Yap</h2>
+    <Form class="sign-in-form row gy-4" :validation-schema="formSchema">
+      <h2>{{ isLoggedActive ? "Giriş Yap" : "Kaydol" }}</h2>
+
       <div class="form-group">
         <Field name="email" v-slot="{ field, errors, errorMessage }">
           <input
+            v-bind="field"
             type="email"
             class="form-control"
-            placeholder="örneğin@gmail.com"
+            placeholder="example@gmail.com"
+            :class="{ 'is-invalid': errors.length !== 0 }"
           />
           <div class="input-alert" v-if="errors.length !== 0">
             {{ errorMessage }}
           </div>
         </Field>
       </div>
+
       <div class="form-group">
         <Field name="password" v-slot="{ field, errors, errorMessage }">
           <input
+            v-bind="field"
             type="password"
             class="form-control"
             placeholder="Şifrenizi giriniz"
+            :class="{ 'is-invalid': errors.length !== 0 }"
           />
           <div class="input-alert" v-if="errors.length !== 0">
             {{ errorMessage }}
           </div>
         </Field>
       </div>
-      <button class="btn btn-outline-primary">Giriş Yap</button>
+
+      <button type="submit" class="btn btn-outline-primary">
+        {{ isLoggedActive ? "Giriş Yap" : "Kaydol" }}
+      </button>
+
+      <div @click="isLoggedActive = !isLoggedActive" class="log-switch">
+        <span v-if="isLoggedActive">Kaydol</span>
+        <span v-else>Giriş Yap</span>
+      </div>
     </Form>
   </div>
 </template>
+
 <script setup>
 import { Field, Form } from "vee-validate";
 import * as yup from "yup";
+import { ref } from "vue";
+
+const formSchema = yup.object({
+  email: yup
+    .string()
+    .required("Zorunlu alan!")
+    .email("Lütfen istenilen formatta giriniz"),
+  password: yup.string().required("Zorunlu alan!"),
+});
+
+const isLoggedActive = ref(true);
 </script>
