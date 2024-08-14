48dc2ea110f22bc0cb8dd67123e620d4427c1cce
Ibrahim Yerli <yrlibrahim25@gmail.com>
2024-08-14 17:51:43 +0300

    New proje start

---
+++ Proje/Movie/movieDashboard/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Header from "./components/Fixed/Header.vue";
+</script>
+
+<template>
+  <div class="container">
+    <Header />
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped></style>
